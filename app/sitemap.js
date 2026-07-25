d13257faa62eafd123a3de5d7e21ec4b2ec21b27
Jamshaid Iqbal <jamshaidiqbal805gmail@gmail.com>
2026-07-26 04:03:28 +0500

    Add sitemap for Google indexing

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://wsroyalbags.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://wsroyalbags.vercel.app/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://wsroyalbags.vercel.app/sales",
+      lastModified: new Date(),
+    },
+  ];
+}
